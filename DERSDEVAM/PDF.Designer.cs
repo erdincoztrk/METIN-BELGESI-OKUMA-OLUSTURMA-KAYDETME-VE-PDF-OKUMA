5a8cfaea6769a1725fde147de811348ffd9d0c03
Erdinç Öztürk <e.erdincoztrk@gmail.com>
2022-07-27 14:11:35 +0300

    Proje dosyası ekle.

---
+++ DERSDEVAM/PDF.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace DERSDEVAM
+{
+    partial class PDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDF));
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(12, 12);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(1116, 764);
+            this.axAcroPDF1.TabIndex = 0;
+            this.axAcroPDF1.Enter += new System.EventHandler(this.axAcroPDF1_Enter);
+            // 
+            // PDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 799);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Name = "PDF";
+            this.Text = "PDF";
+            this.Load += new System.EventHandler(this.PDF_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+    }
+}
---
+++ DERSDEVAM/PDF.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DERSDEVAM
+{
+    public partial class PDF : Form
+    {
+        public PDF()
+        {
+            InitializeComponent();
+        }
+
+        private void axAcroPDF1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PDF_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "PDF|*.pdf";
+            if(of.ShowDialog() == DialogResult.OK)
+            {
+                axAcroPDF1.LoadFile(of.FileName);
+            }
+        }
+    }
+}
